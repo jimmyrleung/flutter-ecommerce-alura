050d048680fa76eef8de1a4640736a0d62078e29
Jimmy Leung <jimmyleung@Jimmys-MacBook-Pro.local>
2020-11-08 18:09:37 -0300

    navigate on cart icon tap

--- lib/app/widgets/cart_button.dart
+++ lib/app/widgets/cart_button.dart
@@ -3,23 +3,30 @@ import 'package:flutter/material.dart';
 class CartButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(100),
-          bottomLeft: Radius.circular(100),
+    // o GestureDetector é um Widget que nos ajuda a incluir
+    // interações. Nesse caso vamos pegar a ação de tap
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, '/cart');
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(100),
+            bottomLeft: Radius.circular(100),
+          ),
         ),
-      ),
-      alignment: Alignment.centerRight,
-      height: 40,
-      padding: EdgeInsets.only(
-        right: 20,
-        left: 20,
-      ),
-      child: Image(
+        alignment: Alignment.centerRight,
         height: 40,
-        image: AssetImage('lib/utils/assets/icones/carrinho.png'),
+        padding: EdgeInsets.only(
+          right: 20,
+          left: 20,
+        ),
+        child: Image(
+          height: 40,
+          image: AssetImage('lib/utils/assets/icones/carrinho.png'),
+        ),
       ),
     );
   }
